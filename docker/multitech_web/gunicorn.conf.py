e800f705d9903211686fc8f09821110d249ad02c
Master Keits <master@furiosa.lan>
2018-07-18 16:32:23 +0300

    /

--- docker/multitech_web/gunicorn.conf.py
+++ docker/multitech_web/gunicorn.conf.py
@@ -2,6 +2,6 @@ from multiprocessing import cpu_count
 
 bind = ['0.0.0.0:8000', '[::0]:8000']
 daemon = False
-workers = cpu_count() * 2 + 1
-# workers = cpu_count() * 1
+# workers = cpu_count() * 2 + 1
+workers = cpu_count() * 1
 max_requests = 1000
